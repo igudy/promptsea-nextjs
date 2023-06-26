16cfe3ebd748b07f080dad1a25479385b732e784
Goodness Igunma <Goodnessigunma1@gmail.com>
2023-06-26 20:05:53 +0100

    Provider sorted out

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,6 @@
-import "@styles/globals.css"
+import "@styles/globals.css";
+import Nav from '@components/Nav';
+import Provider from "@components/Provider";
 
 export const metadata = {
   title: "Promptsea",
@@ -9,11 +11,18 @@ const RootLayout = ({ children }) => {
   return (
     <html lang="en">
       <body>
+        <Provider>
+
         <div className="main">
           <div className="gradient"></div>
         </div>
 
-        <main className="app">{children}</main>
+        <main className="app">
+          <Nav />
+          {children}
+        </main>
+
+        </Provider>
       </body>
     </html>
   )

a83c321a5e095facc929088f3b582d666582941f
Goodness Igunma <Goodnessigunma1@gmail.com>
2023-06-28 00:13:57 +0100

    Route for prompt and user added

--- app/create-prompt/page.jsx
+++ app/create-prompt/page.jsx
@@ -1,4 +1,4 @@
-"use-client"
+"use client"
 
 import { useState } from "react"
 import { useSession } from "next-auth/react"
@@ -7,7 +7,49 @@ import { useRouter } from "next/navigation"
 import Form from "@components/Form"
 
 const CreatePrompt = () => {
-  return <Form />
+  const Router = useRouter()
+  const { data: session } = useSession()
+
+  const [submitting, setSubmitting] = useState(false)
+
+  const [post, setPost] = useState({
+    prompt: "",
+    tag: "",
+  })
+
+  const createPrompt = async (e) => {
+    e.preventDefault()
+    setSubmitting(true)
+
+    try {
+      const response = await fetch("/api/prompt/new", {
+        method: "POST",
+        body: JSON.stringify({
+          prompt: post.prompt,
+          userId: session?.user.id,
+          tag: post.tag,
+        }),
+      })
+
+      if (response.ok) {
+        Router.push("/")
+      }
+    } catch (error) {
+      console.log(error)
+    } finally {
+      setSubmitting(false)
+    }
+  }
+
+  return (
+    <Form
+      type="Create"
+      post={post}
+      setPost={setPost}
+      submitting={submitting}
+      handleSubmit={createPrompt}
+    />
+  )
 }
 
 export default CreatePrompt

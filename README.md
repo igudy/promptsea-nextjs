37d713c907cc8b1ab252938f5602b0d04b61bfa0
Goodness Igunma <Goodnessigunma1@gmail.com>
2023-06-30 23:58:00 +0100

    Tag and search integrated

--- README.md
+++ README.md
@@ -1,17 +1,19 @@
 ### **NextJs Full Stack Web App**
 
-The web app is built using Next.js and utilizes OAuth for authentication. It offers a convenient platform for users to save and manage AI prompts. Users can create and store their own prompts, which serve as inputs for AI-generated content. MongoDB for database.  Credits to Javascript mastery for the youtube course.
-
-
+The web app is built using Next.js and utilizes OAuth for authentication. It offers a convenient platform for users to save and manage AI prompts. Users can create and store their own prompts, which serve as inputs for AI-generated content. MongoDB for database. Credits to Javascript mastery for the youtube course.
 
 ##### Additional Implementation
 
-[]Implement Search
+[]Implement Search - Done
 
-- Search by prompt
-- Search by tag
-- Seardh by username
+- Search by prompt - Done
+- Search by tag - Done
+- Seardh by username - Done
 
 []Implement Click on tag
 
 []IMplement view other profiles
+
+[]Integrate ChatGPT
+
+[]Integrate section(Tech, Story, Movies & More)
--- components/Feed.jsx
+++ components/Feed.jsx
@@ -18,19 +18,52 @@ const PromptCardList = ({ data, handleTagClick }) => {
 }
 
 const Feed = () => {
-  const [searchText, setSearchText] = useState("")
-  const [posts, setPosts] = useState([])
+  const [allPosts, setAllPosts] = useState([])
 
-  const handleSearchChange = (e) => {}
+  // Search states
+  const [searchText, setSearchText] = useState("")
+  const [searchTimeout, setSearchTimeout] = useState(null)
+  const [searchedResults, setSearchedResults] = useState([])
+
+  const fetchPosts = async () => {
+    const response = await fetch("/api/prompt")
+    const data = await response.json()
+    setAllPosts(data)
+  }
 
   useEffect(() => {
-    const fetchPosts = async () => {
-      const response = await fetch("/api/prompt")
-      const data = await response.json()
-      setPosts(data)
-    }
     fetchPosts()
   }, [])
+
+  const filterPrompts = (searchtext) => {
+    const regex = new RegExp(searchtext, "i") // 'i' flag for case-insensitive search
+    return allPosts.filter(
+      (item) =>
+        regex.test(item.creator.username) ||
+        regex.test(item.tag) ||
+        regex.test(item.prompt)
+    )
+  }
+
+  const handleSearchChange = (e) => {
+    clearTimeout(searchTimeout)
+    setSearchText(e.target.value)
+
+    // debounce method
+    setSearchTimeout(
+      setTimeout(() => {
+        const searchResult = filterPrompts(e.target.value)
+        setSearchedResults(searchResult)
+      })
+    )
+  }
+
+  const handleTagClick = (tagName) => {
+    setSearchText(tagName)
+
+    const searchResult = filterPrompts(tagName)
+    setSearchedResults(searchResult)
+  }
 
   return (
     <section className="feed">
@@ -44,7 +77,14 @@ const Feed = () => {
           className="search_input peer"
         />
       </form>
-      <PromptCardList data={posts} handleTagClick={() => {}} />
+      {searchText ? (
+        <PromptCardList
+          data={searchedResults}
+          handleTagClick={handleTagClick}
+        />
+      ) : (
+        <PromptCardList data={allPosts} handleTagClick={handleTagClick} />
+      )}
     </section>
   )
 }
